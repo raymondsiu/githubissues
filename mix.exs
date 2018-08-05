98bbb8dd21e4c578a95668df6563b1a53feda4f1
raymondsiu <ray.siu@outlook.com>
2018-08-05 09:29:44 -0400

    Initial commit.

--- mix.exs
+++ mix.exs
@@ -4,8 +4,11 @@ defmodule Issues.MixProject do
   def project do
     [
       app: :issues,
+      escript: escript_config(),
       version: "0.1.0",
-      elixir: "~> 1.7",
+      name: "Github Issues",
+      source_url: "https://github.com/raymondsiu/githubissues",
+      elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -21,8 +24,18 @@ defmodule Issues.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:httpoison, "~> 1.0.0"},
+      {:poison, "~> 3.1"},
+      {:ex_doc, "~> 0.18.1"},
+      {:earmark, "~> 1.2.4"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+    ]
+  end
+
+  defp escript_config do
+    [
+      main_module: Issues.CLI
     ]
   end
 end
---
+++ test/cli_test.exs
@@ -0,0 +1,34 @@
+defmodule CliTest do
+  use ExUnit.Case
+  doctest Issues
+
+  import Issues.CLI, only: [parse_args: 1, sort_into_descending_order: 1]
+
+  test ":help returned by option parsing with -h and --help options" do
+    # ensure this returns the atom :help
+    assert parse_args(["-h", "anything"]) == :help
+    # ensure this returns the atom :help
+    assert parse_args(["--help", "anything"]) == :help
+  end
+
+  test "three values returned if three given" do
+    assert parse_args(["user", "project", "99"]) == {"user", "project", 99}
+  end
+
+  test "count is defaulted if two values given" do
+    assert parse_args(["user", "project"]) == {"user", "project", 4}
+  end
+
+  test "sort descending orders the correct way" do
+    result = sort_into_descending_order(fake_created_at_list(["c", "a", "b"]))
+    # for each issue in the result, get the created_at value
+    issues = for issue <- result, do: Map.get(issue, "created_at")
+    # check that the resulting issues are in the right order
+    assert issues == ~w{ c b a }
+  end
+
+  defp fake_created_at_list(values) do
+    for value <- values,
+        do: %{"created_at" => value, "other_data" => "xxx"}
+  end
+end
